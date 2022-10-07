29e233a3b5b85e5ad26079c4deda3f11f85eb9ce
echa <bernardus.hersa37@gmail.com>
2022-10-07 18:43:31 +0700

    checkpoint before I give up

--- src/main/java/NumberPuzzle.java
+++ src/main/java/NumberPuzzle.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class NumberPuzzle {
 
     private int[][] numberBoard;
-    private int zeroX;
-    private int zeroY;
+    private int zeroRow;
+    private int zeroColumn;
 
-    public NumberPuzzle(int[][] numberBoard, int zeroX, int zeroY) {
+    public NumberPuzzle(int[][] numberBoard, int zeroRow, int zeroColumn) {
         this.numberBoard = numberBoard;
-        this.zeroX = zeroX;
-        this.zeroY = zeroY;
+        this.zeroRow = zeroRow;
+        this.zeroColumn = zeroColumn;
     }
 
     public NumberPuzzle(int sizeX, int sizeY) {
@@ -15,17 +17,81 @@ public class NumberPuzzle {
         generateRandomBoard();
     }
 
+    public NumberPuzzle(NumberPuzzle board) {
+        numberBoard = copyArray(board.numberBoard);
+        zeroRow = board.zeroRow;
+        zeroColumn = board.zeroColumn;
+    }
+    public int[][] getNumberBoard() {
+        return numberBoard;
+    }
+
+    public void setNumberBoard(int[][] numberBoard) {
+        this.numberBoard = numberBoard;
+    }
+
+    public int getZeroRow() {
+        return zeroRow;
+    }
+
+    public int getZeroColumn() {
+        return zeroColumn;
+    }
     public void generateRandomBoard() {
         int size = numberBoard.length * numberBoard[0].length;
-        int[] array = Larik.generateArrayUniform(size, 0, 1);
+        int[] array = Larik.generateArrayUniform(size, -1, 1);
         array = Larik.randomizeArray(array);
         int index = 0;
         for(int i = 0; i < numberBoard.length; i++){
             for(int j = 0; j < numberBoard[0].length; j++) {
                 numberBoard[i][j] = array[index];
+                if(array[index] == 0) {
+                    zeroRow = i;
+                    zeroColumn = j;
+                }
                 index++;
             }
         }
+    }
+
+    public void left(){
+        if(zeroColumn > 0) {
+            numberBoard[zeroRow][zeroColumn] = numberBoard[zeroRow][zeroColumn-1];
+            numberBoard[zeroRow][zeroColumn - 1] = 0;
+            zeroColumn--;
+        }
+    }
+
+    public void up() {
+        if(zeroRow > 0) {
+            numberBoard[zeroRow][zeroColumn] = numberBoard[zeroRow - 1][zeroColumn];
+            numberBoard[zeroRow - 1][zeroColumn] = 0;
+            zeroRow--;
+        }
+    }
+
+    public void right() {
+        if(zeroColumn < numberBoard[0].length - 1) {
+            numberBoard[zeroRow][zeroColumn] = numberBoard[zeroRow][zeroColumn + 1];
+            numberBoard[zeroRow][zeroColumn + 1] = 0;
+            zeroColumn++;
+        }
+    }
+
+    public void down() {
+        if(zeroRow < numberBoard.length -1) {
+            numberBoard[zeroRow][zeroColumn] = numberBoard[zeroRow + 1][zeroColumn];
+            numberBoard[zeroRow + 1][zeroColumn] = 0;
+            zeroRow++;
+        }
+    }
+
+    public static int[][] copyArray(int[][] array) {
+        int[][] copy = new int[array.length][];
+        for(int i = 0; i < array.length; i++) {
+            copy[i] = Arrays.copyOf(array[i], array[i].length);
+        }
+        return copy;
     }
 
     public void printBoard() {
@@ -34,14 +100,11 @@ public class NumberPuzzle {
             for(int j = 0; j < numberBoard[i].length; j++) {
                 print += numberBoard[i][j] + " ";
             }
+            print +="\n";
         }
         System.out.println(print);
-
     }
 
-    public int[][] getNumberBoard() {
-        return numberBoard;
-    }
 
 
 
